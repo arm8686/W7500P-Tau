0644ca38a5b05cadaff406d94b755b38492cc215
arm8686 <arm86@163.com>
2016-11-18 09:56:34 +0800

    changed EXTERN_XTAL(12000000UL)

--- W7500P-Tau-FW/Libraries/CMSIS/Device/WIZnet/W7500/Include/system_W7500x.h
+++ W7500P-Tau-FW/Libraries/CMSIS/Device/WIZnet/W7500/Include/system_W7500x.h
@@ -1,19 +1,19 @@
 /*******************************************************************************************************************************************************
- * Copyright ¨Ï 2016 <WIZnet Co.,Ltd.> 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ¡°Software¡±), 
- * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * Copyright ¨Ï 2016 <WIZnet Co.,Ltd.>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ¡°Software¡±),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
- * THE SOFTWARE IS PROVIDED ¡°AS IS¡±, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * THE SOFTWARE IS PROVIDED ¡°AS IS¡±, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *********************************************************************************************************************************************************/
 /**************************************************************************/
 /**
- * @file    system_W7500x.h 
+ * @file    system_W7500x.h
  * @author  IOP Team
  * @version V1.0.0
  * @date    01-May-2015
@@ -38,8 +38,8 @@ extern "C" {
 
 #include "W7500x.h"
 
-extern uint32_t SystemCoreClock;     /*!< System Clock Frequency (Core Clock)  */
-extern uint32_t GetSystemClock(void);    /*!< Get System Clock Frequency */
+extern uint32_t SystemCoreClock;            /*!< System Clock Frequency (Core Clock)  */
+extern uint32_t GetSystemClock(void);       /*!< Get System Clock Frequency */
 
 
 /**
@@ -67,8 +67,8 @@ extern void SystemCoreClockUpdate (void);
 /*----------------------------------------------------------------------------
   Define clocks
  *----------------------------------------------------------------------------*/
-#define EXTERN_XTAL     (8000000UL)     /* External Oscillator Frequency        */
-#define INTERN_XTAL     (8000000UL)     /* Internal Oscillator Frequency         */
+#define EXTERN_XTAL     (12000000UL)    /* External Oscillator Frequency      */
+#define INTERN_XTAL     (8000000UL)     /* Internal Oscillator Frequency      */
 
 #ifdef __cplusplus
 }
